0b4fda41c96c83f3d42d877c7b3cc183dcf4093f
janham <janhampino@gmail.com>
2020-12-22 11:47:55 +0900

    firebase接続,put,get成功

--- chat-app/js/script.js
+++ chat-app/js/script.js
@@ -1,8 +1,6 @@
-let firebaseConfig = '';
-
 $.getJSON('firebase.json', (data) => {
     // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-    firebaseConfig = {
+    let firebaseConfig = {
         apiKey: data['apiKey'],
         authDomain: data['authDomain'],
         projectId: data['projectId'],
@@ -11,36 +9,36 @@ $.getJSON('firebase.json', (data) => {
         appId: data['appId'],
         measurementId: data['measurementId'],
     }
-});
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-firebase.analytics();
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
 
-// firebaseへの処理
-const newPostRef = firebase.database();
-let room = 'room1';
+    // firebaseへの処理
+    const newPostRef = firebase.database();
+    let room = 'room1';
 
-const send = document.getElementById('send');
-const username = document.getElementById('username');
-const text = document.getElementById('text');
-const output = document.getElementById('output');
+    const send = document.getElementById('send');
+    const username = document.getElementById('username');
+    const text = document.getElementById('text');
+    const output = document.getElementById('output');
 
-send.addEventListener('click', function() {
-    newPostRef.ref(room).push({
-        username: username.value,
-        text: text.value
+    send.addEventListener('click', function() {
+        newPostRef.ref(room).push({
+            username: username.value,
+            text: text.value
+        });
+        text.value = "";
     });
-    text.value = "";
-});
 
-newPostRef.ref(room).on('child_added', function(data){
-    const v = data.val();
-    const k = data.key;
-    let str = '';
+    newPostRef.ref(room).on('child_added', function(data){
+        const v = data.val();
+        const k = data.key;
+        let str = '';
 
-    str += '<div class="name">' + v.username + '</div>';
-    str += '<div class="text">' + v.text + '</div>';
+        str += '<div class="name">' + v.username + '</div>';
+        str += '<div class="text">' + v.text + '</div>';
 
-    output.innerHTML += str;
+        output.innerHTML += str;
+    });
 });
